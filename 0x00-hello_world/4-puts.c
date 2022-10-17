130dbc5cb53d91018dad41441e92904d2ceabbb6
root <patriciatatsinkou@gmail.com>
2022-10-17 05:55:50 -0700

    commit3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,8 @@
  * main - prints something on the screen
  * Return: 0 if success.
  */
-int main(void)  
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
