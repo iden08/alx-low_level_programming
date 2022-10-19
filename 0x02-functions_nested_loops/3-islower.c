30d79fa886e32bfca4607ba65b28d5ee66d441be
root <patriciatatsinkou@gmail.com>
2022-10-19 06:54:33 -0700

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower -checks if the character c is lower or uper
+ * @c : the parameter of a function
+ *
+ * Return: 0 success
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
+
