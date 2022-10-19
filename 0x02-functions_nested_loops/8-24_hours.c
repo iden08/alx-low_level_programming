24e9b2f5f7ed83d89035d0a7e3b73c4f305c94ba
root <patriciatatsinkou@gmail.com>
2022-10-19 11:31:50 -0700

    print time of federal agent jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,15 +9,16 @@
 void jack_bauer(void)
 {
 	int h1, h2, m1, m2;
+
 	for (h1 = 0; h1 <= 2; h1++)
 	{
 		for (h2 = 0; h2 <= 9; h2++)
 		{
-			if ((h1 <= 1 && h2 <= 9) || (h1 <=2 && h2 <= 3))
+			if ((h1 <= 1 && h2 <= 9) || (h1 <= 2 && h2 <= 3))
 			{
 				for (m1 = 0; m1 <= 5; m1++)
 				{
-					for (m2 = 0; m2 <= 9;m2++)
+					for (m2 = 0; m2 <= 9; m2++)
 					{
 						_putchar(h1 + '0');
 						_putchar(h2 + '0');
