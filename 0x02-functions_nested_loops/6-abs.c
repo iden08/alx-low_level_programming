605f9f9b60e69f368a74864e26eb843edb2cd712
root <patriciatatsinkou@gmail.com>
2022-10-19 08:51:43 -0700

    the absolue value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value of an integer
+ * @n : the parameter
+ *
+ * Return: 0 success
+ */
+
+int _abs(int)
+{
+	if (n < 0)
+		n = -n;
+
+	return (n);
+}
+
