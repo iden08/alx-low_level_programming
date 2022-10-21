762efa8862a8e34965a062065834f5bedf0a9abf
root <patriciatatsinkou@gmail.com>
2022-10-21 09:30:24 -0700

    product

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - do the multiplication of two intergera
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: the produit
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
