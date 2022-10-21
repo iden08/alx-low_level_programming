d7d791cf72903f0f73999bab2968b5870e18f2c9
root <patriciatatsinkou@gmail.com>
2022-10-21 10:06:09 -0700

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,12 +10,12 @@ void print_most_numbers(void)
 {
 	int n;
 
-	for (n =0; n<= 9; n++)
+	for (n = 0; n <= 9; n++)
 	{
 		if ((n == 2) || (n == 4))
 			continue;
 		else
-			_putchar(n + '0'0);
+			_putchar(n + '0');
 	}
 	_putchar('\n');
 }
