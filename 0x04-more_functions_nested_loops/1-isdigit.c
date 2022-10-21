6be17401939772bde10479a3278a8cda5b1c8912
root <patriciatatsinkou@gmail.com>
2022-10-21 08:57:39 -0700

    is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit
+ * @c: parameter
+ *
+ * Return: 1 if digit 0 else
+ */
+
+int _isdigit(int c)
+
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
+
