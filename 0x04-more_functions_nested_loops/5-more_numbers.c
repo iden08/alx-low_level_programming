19b89b3356aa2d8c176973e6a8fbf64ead8cd0cc
root <patriciatatsinkou@gmail.com>
2022-10-21 10:11:04 -0700

    more mumbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * more_numbers - more numbers
+ *
+ * Return: nothing
+ */
+
+void more_numbers(void)
+{
+	int x, y;
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y > 9)
+			{
+				_putchar((y / 10) + '0');
+			}
+
+			_putchar((y % 10) + '0');
+
+
+		}
+
+		_putchar('\n');
+	}
+}
+
